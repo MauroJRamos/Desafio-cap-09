982bfdbff28856b388b8c1620b95dad1c8b96428
MauroJRamos <mauroramosti@gmail.com>
2022-01-02 12:12:57 -0300

    HasAnyHoles

--- ws-movieflix/frontend/src/pages/MoviesDetails/index.tsx
+++ ws-movieflix/frontend/src/pages/MoviesDetails/index.tsx
@@ -6,65 +6,57 @@ import { SpringPage } from 'types/vendor/spring';
 import { hasAnyRoles } from 'util/auth';
 import { requestBackend } from 'util/requests';
 
-
 import './styles.css';
 
-
 const MoviesDetails = () => {
-  
- 
   const [page, setPage] = useState<SpringPage<Reviews>>();
- 
+
   useEffect(() => {
-    const params : AxiosRequestConfig = {
+    const params: AxiosRequestConfig = {
       url: '/movies/1/reviews',
       withCredentials: true,
-      
     };
- 
+
     requestBackend(params).then((response) => {
       setPage(response.data);
       console.log(page);
     });
   }, []);
 
-
-  
-
   return (
     <div className="movie-container">
       <h1 className="">Tela detalhes do filme id:</h1>
       {hasAnyRoles(['ROLE_MEMBER']) && (
-      <form className="base-card">
-        <div className="form-container-content base-card">
-          <div className="form-container">
-            <input
-              type="text"
-              name="git"
-              className="search-input form-control"
-              placeholder="Deixe sua avaliação aqui"
-            />
-            <button type="submit" className="btn btn-primary search-button">
-              SALVAR AVALIAÇÃO
-            </button>
+        <form className="base-card">
+          <div className="form-container-content base-card">
+            <div className="form-container">
+              <input
+                type="text"
+                name="git"
+                className="search-input form-control"
+                placeholder="Deixe sua avaliação aqui"
+              />
+              <button type="submit" className="btn btn-primary search-button">
+                SALVAR AVALIAÇÃO
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
       )}
-    
-      <div className="movie-card base-card" >
+
+      <div className="movie-card base-card">
         <div className="movie-card-user">
           <UserImage />
-          <p >Marina</p>
+          <p>Marina</p>
         </div>
         <div className="movie-card-text">
-           <div>
-              <label htmlFor="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
-           </div>
+          <div>
+            <label htmlFor="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </label>
+          </div>
         </div>
-        
       </div>
-    
     </div>
   );
 };
